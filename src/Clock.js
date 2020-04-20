54bed6509f80a2d06752378a23b091844dd13b16
Ollie Lee <myself.ollie.lee@gmail.com>
2020-04-20 21:28:21 +0930

    fail to call all countries

--- src/Clock.js
+++ src/Clock.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import './clock.css';
 
+
 class Clock extends React.Component {
   constructor(props) {
     super(props);
@@ -38,9 +39,11 @@ class Clock extends React.Component {
       <div className='clock'>
         <div className='clock-date'><span>{this.state.day} {this.state.year}</span></div>
         <div className='clock-time'><span className='clock-time-hour'>{this.state.hour}</span> : <span className='clock-time-min'>{this.state.min}</span> : <span className='clock-time-sec'>{this.state.sec}</span></div>
+
       </div>
     );
   }
+  
 }
 
 export default Clock;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Clock from './Clock';
+import Clock from './Clock.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
